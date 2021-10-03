53310bcb9a6733dc9f9e8cce56317232baab0f61
Naresh <25naresh@gmail.com>
2021-10-03 19:45:53 +1100

    location tab

--- force-app/main/default/lwc/locationTab/locationTab.js
+++ force-app/main/default/lwc/locationTab/locationTab.js
@@ -1,3 +1,48 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getLocationInformation from '@salesforce/apex/CTLocationTabController.getLocationInformation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+const columns = [
+    { label: 'Token', fieldName: 'token', type: 'text' },
+    { label: 'Contact Status', fieldName: 'status', type: 'text' },
+    { label: 'Visit Date', fieldName: 'visitDate', type: 'date' }
+];
+export default class LocationTab extends LightningElement {
 
-export default class LocationTab extends LightningElement {}
+    @track
+    locationinfo;
+    columns = columns;
+
+    fetchLocationInformation(recordId) {
+        getLocationInformation({ recordId: recordId }).then(result => {
+            if (result && result.name) {
+                this.locationinfo = result;
+            } else {
+                this.showToast("ERROR", "Please enter valid location id", "error");
+            }
+        }).catch(error => console.log("error in fetching location info" + error));
+    }
+
+    handleSearch() {
+        this.locationinfo = null;
+        const recordId = this.template.querySelector("lightning-input");
+        const recordIdValue = recordId.value;
+        if (recordIdValue) {
+            this.fetchLocationInformation(recordIdValue);
+        } else {
+            this.showToast("ERROR", "Please enter valid location id", "error");
+        }
+
+    }
+
+    get headerClass() {
+        return this.locationinfo?.status ? this.locationinfo.status + ' health-header' : 'health-header';
+    }
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(event);
+    }
+}
